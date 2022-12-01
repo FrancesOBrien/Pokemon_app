0317801f78af376c4a95a3472e809aeb0ae37a9a
Frances O'Brien <jfrancesobrien@gmail.com>
2022-12-01 13:08:11 -0500

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 const port = 3000
 const pokemon = require('./models/pokemon.js')
+// const index = require('./views/Index.jsx')
+
+app.engine('jsx', (filePath, options, callback) => { 
+    fs.readFile(filePath, (err, content) => {
+      if (err) return callback(err)
+      const rendered = content.toString()
+      return callback(null, rendered)
+    })
+  })
+  app.set('views', './views') 
+  app.set('view engine', 'jsx') 
+  
 
 //get route welcome
 app.get('/', (req, res) => {
@@ -9,7 +22,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/pokemon', (req, res) => {
-    res.send(pokemon)
+    res.render('Index')
 })
 
 
